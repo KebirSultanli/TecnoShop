698d08eb0c3ec67a81ee5ba61bb66971cedf4f3c
KebirSultanli <kebir.sultanli2020@gmail.com>
2024-08-04 01:43:56 +0400

    Shop Promotion

---
+++ script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-btn-next",
+        prevEl: ".swiper-btn-prev",
+    },
+  });
+
+
